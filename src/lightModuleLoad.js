c68179c0416a229bd5c36df027136290b5f5c297
wjj <13524220689@189.cn>
2016-03-27 22:42:18 +0800

    测试

--- src/lightModuleLoad.js
+++ src/lightModuleLoad.js
@@ -1,3 +1,20 @@
-/*
-*
-* */
+/**
+ * @name lightModuleLoad
+ * @description 轻量模块加载器
+ * @author zengwenbin
+ * @email zwenbin1991@163.com
+ * @date 2016-3-27
+ * @version 1.0.0
+ */
+
+;(function (root, factory) {
+    root.lightModuleLoad = factory();
+})(this, function () {
+    'use strict';
+
+    /**
+     * @type {Object}
+     * 正则表达式
+     */file:///d: http://
+    var protocolExp = /(file:\/*?[a-z]:|http:\/+)/i;
+});
